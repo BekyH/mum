8456a6b09329241a47e8c9059848be6d2fb431d2
bereketheramo25@gmail.com <bereketheramo25@gmail.com>
2021-07-06 16:21:23 +0300

    maxcount java

---
+++ day4/countmax.java
@@ -0,0 +1,24 @@
+public class countmax {
+
+    public static void main(String[] args){
+        int [] a = {1,2,3,4,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,6,3,4,5,6,4,5,6,5,6,3,2,1,2,4,6};
+        System.out.println(Maxcount(a));
+    }
+    public static int Maxcount(int[] a){
+        int maxval = Integer.MIN_VALUE;
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i]>maxval){
+                maxval = a[i];
+                
+            }
+        }
+        int counter=0;
+        for(int i=0;i<a.length;i++){
+            if(maxval==a[i]){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
