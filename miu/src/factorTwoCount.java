cadbf23c2f2fe5cbebcf742d184599bc60e0b7d3
BekyH <bereketheramo25@gmail.com>
2022-10-09 13:31:01 +0300

    factor two cout

---
+++ miu/src/factorTwoCount.java
@@ -0,0 +1,25 @@
+public class factorTwoCount {
+    public static void main(String[] args) {
+    int n = 1000;
+    System.out.println(factorCount(n));
+    }
+
+    // question
+    /** Write a function named factorTwoCount that returns the number of times that 2 divides the argument.
+     For example, factorTwoCount(48) returns 4 because
+     48/2 = 24
+     24/2 = 12
+     12/2 = 6
+     6/2 = 3
+     2 does not divide 3 evenly.
+     Another example: factorTwoCount(27) returns 0 because 2 does not divide 27.**/
+    public static int factorCount(int n){
+      int counter = 0;
+      while(n%2==0){
+         counter++;
+         n  = n/2;
+      }
+
+      return counter;
+    }
+}
